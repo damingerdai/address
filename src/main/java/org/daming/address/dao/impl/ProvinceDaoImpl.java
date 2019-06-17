718b4b3c7440fdca4e765cd43fc577d9e6dc0a26
Arthur Ming <guobin.ming@pwc.com>
2019-06-17 08:55:28 +0800

    fix: 修复不能读取本地资源文件

--- src/main/java/org/daming/address/dao/impl/ProvinceDaoImpl.java
+++ src/main/java/org/daming/address/dao/impl/ProvinceDaoImpl.java
@@ -3,12 +3,18 @@ package org.daming.address.dao.impl;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.daming.address.dao.IProvinceDao;
 import org.daming.address.pojo.Province;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Repository;
 import org.springframework.util.ResourceUtils;
 import org.apache.commons.io.FileUtils;
 
 import javax.annotation.PostConstruct;
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.List;
@@ -33,9 +39,16 @@ public class ProvinceDaoImpl implements IProvinceDao  {
 
     @PostConstruct
     private void init() throws Exception {
-        File jsonFile = ResourceUtils.getFile("classpath:provinces.json");
-        String content = FileUtils.readFileToString(jsonFile, Charset.defaultCharset());
+        Resource resource = new ClassPathResource("provinces.json");
+        StringBuilder sb = new StringBuilder();
+        try (InputStream is = resource.getInputStream();BufferedReader br = new BufferedReader(new InputStreamReader(is, "UTF-8"))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                sb.append(line);
+            }
+        }
 
+        String content = sb.toString();
         this.provinces = this.jsonMapper.readValue(content, List.class);
     }
 
